167d4729af8ff4eef82e06ede2e507677010e744
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-05-10 14:11:05 +0100

    count the number of planes at the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,4 +9,13 @@ class Airport
     puts 'A plane has landed at the airport!'
     @planes << @plane
   end
+
+  def take_off
+    puts 'A plane has departed the airport!'
+    @planes.pop
+  end
+
+  def plane_count
+    @planes.count
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,7 +6,16 @@ describe Airport do
     it 'allows the airport to receive a plane' do
       plane = Plane.new
       airport = Airport.new(plane)
-      expect(airport.land_plane).to include(plane)
+      expect { airport.land_plane }.to change { airport.plane_count }.by(1)
+    end
+  end
+
+  describe '#take_off' do
+    it 'instructs a plane to take off and confirm departure' do
+      plane = Plane.new
+      airport = Airport.new(plane)
+      airport.land_plane
+      expect { airport.take_off }.to change { airport.plane_count }.by(-1)
     end
   end
 end
